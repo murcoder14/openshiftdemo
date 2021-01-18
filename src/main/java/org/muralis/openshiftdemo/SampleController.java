c4ed335e202b592f60ebf67de3082cea7cb77a85
Murali, Therthala <therthala.murali@anthem.com>
2021-01-17 22:40:34 -0500

    Fourth Commit

--- src/main/java/org/muralis/openshiftdemo/SampleController.java
+++ src/main/java/org/muralis/openshiftdemo/SampleController.java
@@ -4,13 +4,16 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.ZoneId;
+
 @RestController
 @EnableAutoConfiguration
 public class SampleController {
 
     @RequestMapping("/")
     String home() {
-        String currentDtTm = java.time.LocalDateTime.now().toString();
+        ZoneId zoneId = ZoneId.of("America/New_York");
+        String currentDtTm = java.time.LocalDateTime.now(zoneId).toString();
         return "HI, WELCOME TO REDHAT OPENSHIFT! " + currentDtTm;
     }
 
